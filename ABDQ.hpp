a01340ef7ad97e33f3bc74b5f23a7f041d3abe60
Dylan G. <69050896+dylan-griffin@users.noreply.github.com>
2025-11-14 04:17:51 +0000

    Fix variable name inconsistencies in ABDQ constructor

--- ABDQ.hpp
+++ ABDQ.hpp
@@ -11,9 +11,9 @@ class ABDQ : public DequeInterface<T>
 {
 public:
     ABDQ() : data_(new T[4]), capacity_(4), size_(0), front_(0), back_(0) {}
-    explicit ABDQ(const size_t capacity) : capacity_(capacity), curr_size_(0)
+    explicit ABDQ(const size_t capacity) : capacity_(capacity), size_(0), front_(0), back_(0)
     {
-        array_ = new T[capacity_];
+        data_ = new T[capacity_];
     }
     ABDQ(const ABDQ &other) : data_(new T[other.capacity_]), capacity_(other.capacity_), size_(other.size_), front_(0), back_(other.size_)
     {
